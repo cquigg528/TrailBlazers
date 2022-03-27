19abaec8bed9c5d7520ddd95886d80ea027aeada
cquigg <cjq425@gmail.com>
2022-03-26 22:13:58 -0700

    added database connection class

---
+++ src/controller/Trail.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import ca.ubc.cs304.model.BranchModel;
+
+/**
+ * This class handles all database related transactions
+ *
+ * Code adapted from cs304 JavaDemo file
+ */
+public class DatabaseConnectionHandler {
+
+    // Use this version of the ORACLE_URL if you are running the code off of the server
+//	private static final String ORACLE_URL = "jdbc:oracle:thin:@dbhost.students.cs.ubc.ca:1522:stu";
+
+    // Use this version of the ORACLE_URL if you are tunneling into the undergrad servers
+    private static final String ORACLE_URL = "jdbc:oracle:thin:@localhost:1522:stu";
+    private static final String EXCEPTION_TAG = "[EXCEPTION]";
+    private static final String WARNING_TAG = "[WARNING]";
+
+    private Connection connection = null;
+
+    public DatabaseConnectionHandler() {
+        try {
+            // Load the Oracle JDBC driver
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+    }
+}
